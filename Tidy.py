497993c43da50b16f2e0a8340d8d10350d1063eb
Mohammed Jasam <mnqnd@mst.edu>
2017-04-08 08:27:40 -0500

    Spoiled the Logic

--- Tidy.py
+++ Tidy.py
@@ -18,12 +18,20 @@ def logic(i,s):
     temp=s
 
     for i in range(len(s),1,-1):
-        print(s)
-        print(i)
+        # print(s)
+        # print(i)
         x=i-1
+
         if s[x-2]>=s[x-1]:
-            print(s[x-1],s[x-1])
+            # print(s[x-1],s[x-1])
             if i>-1:
+                if s[x-1]==0 and s[x]==0:
+                    s[x-1]=9
+                    s[x]=9
+                    if (s[x-2]-1)<0:
+                        s[x-2]==0
+                    else:
+                        s[x-2]-=1
                 while s[x-1]>s[x]:
                     aj=s[x-1]
                     ai=s[x]
@@ -43,8 +51,9 @@ def logic(i,s):
             x1=str(BruteForce(i,x1))
             s[x-1]=int(x1[0])
             s[x]=int(x1[1])
-    return logic(i,s)
-
+    # return logic(i,s)
+    if s[-1]<9:
+        s[-1]=9
     return s
 
 
@@ -59,13 +68,29 @@ for i in range(n):
     s=input()
     temp=int(s)
     s=[int(x) for x in list(s)]
+    # if s[-1]==0 and s[-2]==0:
 
 
     if len(s)>2:
         res=logic(i+1,s)
-        print("Final Result is")
-        print(res)
+
+
+        # print("Final Result is")
+        r1=""
+        if res[0]==0:
+            del res[0]
+
+        aahaa=True
+        while aahaa:
+            res=logic(i+1,res)
+            if res.index(min(res))==0:
+                aahaa=False
+                break
+
+        for x in res:
+            r1+=str(x)
+        print("Case #{0}: {1}".format(i+1, r1))
     elif len(s)==2:
         res=ordertest(i+1,temp)
     elif len(s)==1:
-        print(s[0])
+        print("Case #{0}: {1}".format(i+1, s[0]))
